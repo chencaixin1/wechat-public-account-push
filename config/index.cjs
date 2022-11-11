ddb0ffa516720cde88ceb8bee6f356573c588152
chencaixin1 <117954714+chencaixin1@users.noreply.github.com>
2022-11-11 08:41:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '大宝贝漂亮依依崽',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o7scX6PYQkaTB2r8_7mUulkguYiE',
+      id: 'o7scX6BzTnTPtJSHAT9B1hiGy2ls',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'wQBgJPK-fJnOCpsi_ZhelYTKC_CTu75-_ZuXuiUUMzQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'gSm5dA39hESjoVq4S0uFKLaStemLqvLuKTJ14uQpJLI',
+  CALLBACK_TEMPLATE_ID: 'MnUowfLi4FQM044KvJFaDwBMXCd13Mo2kY_BWP38pnU',
 
   CALLBACK_USERS: [
     {
